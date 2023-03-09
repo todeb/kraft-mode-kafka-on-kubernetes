c65b7152ede233b7e75069493e5b695bf2030627
todeb <61552048+todeb@users.noreply.github.com>
2023-03-09 22:05:41 +0100

    additional variables supported for external access
    ADD_LISTENERS , ADD_ADVERTISED_LISTENERS, ADD_LISTENER_SECURITY_PROTOCOL_MAP are configurable now.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,9 @@ NODE_ID=${HOSTNAME:6}
 LISTENERS="PLAINTEXT://:9092,CONTROLLER://:9093"
 ADVERTISED_LISTENERS="PLAINTEXT://kafka-$NODE_ID.$SERVICE.$NAMESPACE.svc.cluster.local:9092"
 
+if [ -n "$ADD_LISTENERS" ]; then LISTENERS+=,$ADD_LISTENERS ; fi
+if [ -n "$ADD_ADVERTISED_LISTENERS" ]; then ADVERTISED_LISTENERS+=,$ADD_ADVERTISED_LISTENERS; fi
+
 CONTROLLER_QUORUM_VOTERS=""
 for i in $( seq 0 $REPLICAS); do
     if [[ $i != $REPLICAS ]]; then
@@ -21,6 +24,10 @@ sed -e "s+^node.id=.*+node.id=$NODE_ID+" \
 -e "s+^advertised.listeners=.*+advertised.listeners=$ADVERTISED_LISTENERS+" \
 -e "s+^log.dirs=.*+log.dirs=$SHARE_DIR/$NODE_ID+" \
 /opt/kafka/config/kraft/server.properties > server.properties.updated
+
+if [ -n "$ADD_LISTENER_SECURITY_PROTOCOL_MAP" ]; then
+  sed -Ei "s/(^listener\.security\.protocol\.map=.*)/\1,$ADD_LISTENER_SECURITY_PROTOCOL_MAP/" server.properties.updated
+fi
 
 cat <<EOF >> server.properties.updated
 default.replication.factor=${DEFAULT_REPLICATION_FACTOR:=3}
